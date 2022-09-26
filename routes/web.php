0ba7b52b93f3680d28acefd7a8b704fb5fc6c371
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-26 18:03:32 +0200

    changed route in home.blade

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('admin.home');
+Route::get('/admin', 'HomeController@index')->name('admin.home');
