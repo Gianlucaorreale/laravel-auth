86919b6b2dcae8062b94712c7e6660b5ea4b5123
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-28 16:43:14 +0200

    added distroy in PostController

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,10 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<header>
-    
-    <div class="container">
-       <h1>{{$post->title}}</h1>
+<div class="container">
+   <header>
+       <h1>{{$post->title}}</h1>    
+   </header>
+   <div class="clearfix">
+      @if($post->image)
+      <img class="float-left mr-2" src="{{$post->image}}" alt="{{$post->slug}}"> 
+      @endif
+      <p>{{$post->content}}</p>
+   <div>
+       <time>Creato il: {{$post->created_at}}</time>
+   </div>
+   <div>
+       <time>Modificato il: {{$post->updated_at}}</time>
+   </div>
+   </div>
+   <hr>
+   <footer class="d-flex align-items-center justify-content-between">
+    <div>
+        <a href="{{route('admin.posts.index')}}" class="btn btn-secondary">
+            <i class=" fa-solid fa-rotate-left mr-2"></i>indietro
+        </a>
     </div>
-</header>
+    <div class="d-flex align-items-center justify-content-end">
+         <form action="{{ route('admin.posts.destroy', $post->id ) }}" method="POST">
+            @csrf
+            @method('DELETE')
+             <button class="btn btn-danger" type="submit">
+                 <i class="fa-solid fa-trash mr-2"></i>Elimina
+             </button>
+         </form>
+    </div>
+   </footer>
+</div>
 @endsection
